6c484836ef072937e95ecdbc9e99c83bbb927f19
Nieves Borrero <nievesborrerobarea@gmail.com>
2023-01-19 19:18:45 +0100

    feat: focus trap

--- src/components/accessible-modal/ConfirmationModal.tsx
+++ src/components/accessible-modal/ConfirmationModal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 import { IoMdClose } from "react-icons/io";
 
@@ -30,24 +30,81 @@ export const ConfirmationModal = ({
   onClose,
   onAccept,
 }: ConfirmationModalProps) => {
-  const [activeElement, setActiveElement] = useState(document.activeElement);
+  const activeElement = document.activeElement;
+
   const ref = useRef<HTMLDivElement>(null);
-  const acceptButtonRef = useRef<HTMLButtonElement>(null);
+  const cancelButtonRef = useRef<HTMLButtonElement>(null);
 
   useEffect(() => {
-    setActiveElement(document.activeElement);
-    acceptButtonRef.current?.focus();
+    cancelButtonRef.current?.focus();
   }, []);
 
-  const close = (event: ClickEvent) => {
-    event.preventDefault();
+  const close = useCallback(
+    (event: ClickEvent | KeyboardEvent) => {
+      event.preventDefault();
+      event.stopPropagation();
 
-    if (activeElement instanceof HTMLElement) {
-      activeElement.focus();
+      if (activeElement instanceof HTMLElement) {
+        activeElement.focus();
+      }
+
+      onClose();
+    },
+    [activeElement, onClose]
+  );
+
+  const trapFocus = (event: KeyboardEvent) => {
+    if (!ref.current) {
+      return;
     }
 
-    onClose();
+    const focusableElements = ref.current.querySelectorAll(
+      `a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), 
+      input:not([disabled]), select:not([disabled]), div[role="button"]`
+    );
+
+    const firstElement = focusableElements[0];
+    const lastElement = focusableElements[focusableElements.length - 1];
+
+    if (
+      !event.shiftKey &&
+      firstElement instanceof HTMLElement &&
+      document.activeElement === lastElement
+    ) {
+      firstElement.focus();
+      return event.preventDefault();
+    }
+
+    if (
+      event.shiftKey &&
+      lastElement instanceof HTMLElement &&
+      document.activeElement === firstElement
+    ) {
+      lastElement.focus();
+      event.preventDefault();
+    }
   };
+
+  const handleKeyDown = useCallback(
+    (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        close(event);
+      }
+
+      if (event.key === "Tab") {
+        trapFocus(event);
+      }
+    },
+    [close]
+  );
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [handleKeyDown]);
 
   useEffect(() => {
     const listener = (event: MouseEvent | TouchEvent) => {
@@ -68,12 +125,8 @@ export const ConfirmationModal = ({
         ref={ref}
         role="dialog"
         aria-modal
-        // TODO 
-        aria-label="title"
         aria-labelledby="title"
         aria-describedby="desc"
-        // TODO
-        tabIndex={-1}
       >
         <SectionsWrapper>
           <Header>
@@ -83,25 +136,16 @@ export const ConfirmationModal = ({
             <p id="desc">{description}</p>
           </Content>
           <Footer>
-            <button
-              onClick={onAccept}
-              ref={acceptButtonRef}
-              aria-label="aceptar"
-            >
-              Aceptar
-            </button>
-            <button onClick={close} aria-label="Cancelar">
+            <button ref={cancelButtonRef} onClick={close} aria-label="Cancelar">
               Cancelar
+            </button>
+            <button onClick={onAccept} aria-label="aceptar">
+              Aceptar
             </button>
           </Footer>
         </SectionsWrapper>
         <IconWrapper>
-          <div
-            role="button"
-            onClick={close}
-            aria-label="cerrar modal"
-            tabIndex={0}
-          >
+          <div role="button" onClick={close} aria-label="cerrar" tabIndex={0}>
             <IoMdClose aria-hidden="true" />
           </div>
         </IconWrapper>
